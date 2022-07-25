fe0c1552add7434584d2a297efb71d2b7a92e27d
rajprem4214 <rajprem4214@gmail.com>
2022-07-25 10:17:19 +0530

    new

--- mat.cpp
+++ mat.cpp
@@ -12,7 +12,7 @@ int main()
 
     // If column of first matrix in not equal to row of second matrix,
     // ask the user to enter the size of matrix again.
-    while (c1!=r2)
+    while (c1 != r2)
     {
         cout << "Error! column of first matrix not equal to row of second.";
 
@@ -24,47 +24,50 @@ int main()
     }
 
     // Storing elements of first matrix.
-    cout << endl << "Enter elements of matrix 1:" << endl;
-    for(i = 0; i < r1; ++i)
-        for(j = 0; j < c1; ++j)
+    cout << endl
+         << "Enter elements of matrix 1:" << endl;
+    for (i = 0; i < r1; ++i)
+        for (j = 0; j < c1; ++j)
         {
             cout << "Enter element a" << i + 1 << j + 1 << " : ";
             cin >> a[i][j];
         }
 
     // Storing elements of second matrix.
-    cout << endl << "Enter elements of matrix 2:" << endl;
-    for(i = 0; i < r2; ++i)
-        for(j = 0; j < c2; ++j)
+    cout << endl
+         << "Enter elements of matrix 2:" << endl;
+    for (i = 0; i < r2; ++i)
+        for (j = 0; j < c2; ++j)
         {
             cout << "Enter element b" << i + 1 << j + 1 << " : ";
             cin >> b[i][j];
         }
 
     // Initializing elements of matrix mult to 0.
-    for(i = 0; i < r1; ++i)
-        for(j = 0; j < c2; ++j)
+    for (i = 0; i < r1; ++i)
+        for (j = 0; j < c2; ++j)
         {
-            mult[i][j]=0;
+            mult[i][j] = 0;
         }
 
     // Multiplying matrix a and b and storing in array mult.
-    for(i = 0; i < r1; ++i)
-        for(j = 0; j < c2; ++j)
-            for(k = 0; k < c1; ++k)
+    for (i = 0; i < r1; ++i)
+        for (j = 0; j < c2; ++j)
+            for (k = 0; k < c1; ++k)
             {
                 mult[i][j] += a[i][k] * b[k][j];
             }
 
     // Displaying the multiplication of two matrix.
-    cout << endl << "Output Matrix: " << endl;
-    for(i = 0; i < r1; ++i)
-    for(j = 0; j < c2; ++j)
-    {
-        cout << " " << mult[i][j];
-        if(j == c2-1)
-            cout << endl;
-    }
+    cout << endl
+         << "Output Matrix: " << endl;
+    for (i = 0; i < r1; ++i)
+        for (j = 0; j < c2; ++j)
+        {
+            cout << " " << mult[i][j];
+            if (j == c2 - 1)
+                cout << endl;
+        }
 
     return 0;
 }
